3829e5bebc6549491502e6fb96193bb16ac4bf59
Rishabh Verma <rishabhv11@gmail.com>
2020-08-08 03:21:33 +0530

    Version 2

--- js/script.js
+++ js/script.js
@@ -1,150 +1,80 @@
+// loader icon fades first, then the background
 $(window).on("load", function() {
+    $(".loader .inner").fadeOut(500, function() {
+      $(".loader").fadeOut(750);
+    });
+  });
+  
+  // https://github.com/nicinabox/superslides
+  $(document).ready(function(){
+    $('#slides').superslides();
+  
+  // https://github.com/mattboldt/typed.js/
+    let typed = new Typed(".typed", {
+      strings: ["Aspiring Data Scientist"],
+      typeSpeed: 70,
+      // loop: true,
+      startDelay: 1000,
+      showCursor: false
+    });
+  
+  // https://owlcarousel2.github.io/OwlCarousel2/demos/basic.html
+  $('#owl-one').owlCarousel({
+    loop:true,
+    items:4,
+    itemsDesktop:[1000,4],
+    itemsDesktopSmall:[900,3],
+    itemsTablet:[600,2],
+    itemsMobile:[480,1],
+    autoplay: true
+  });
 
-	$(".loader .inner").fadeOut(500, function() {
-		$(".loader").fadeOut(750);
-	});
+  
+  $('#owl-two').owlCarousel({
+    loop:true,
+    items: 1,
+    dots: true,
+    lazyLoad: true,
+    size: 300
+  });
 
-})
-
-
-
-
-$(document).ready(function() {
-
-	$('#slides').superslides({
-		animation: 'fade',
-		play: 5000,
-		pagination: false
-	});
-
-	var typed = new Typed(".typed", {
-		strings: ["Data Science Enthusiast.", "Web Developer.", "Student.","Fitness Enthusiast"],
-		typeSpeed: 100,
-		loop: true,
-		startDelay: 1000,
-		showCursor: false
-	});
-
-	$('.owl-carousel').owlCarousel({
-	    loop:true,
-	    items: 4,
-	    responsive:{
-	        0:{
-	            items:1
-	        },
-	        480:{
-	            items:2
-	        },
-	        768:{
-	            items:3
-	        },
-	        938:{
-	            items:4
-	        }
-	    }
-	});
-
-
-	
+  $('#owl-three').owlCarousel({
+    loop:true,
+    items: 1,
+    dots: true,
+    lazyLoad: true,
+    size: 300
+  });
 
 
-	var skillsTopOffset = $(".skillsSection").offset().top;
-
-	$(window).scroll(function() {
-
-		if(window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
-
-			$('.chart').easyPieChart({
-		        easing: 'easeInOut',
-		        barColor: '#D4A59A',
-		        trackColor: false,
-		        scaleColor: false,
-		        lineWidth: 4,
-		        size: 152,
-		        onStep: function(from, to, percent) {
-		        	$(this.el).find('.percent').text(Math.round(percent));
-		        }
-		    });
-			
-
-		}
-
-
-	});
-
-
-	$("#filters a").click(function() {
-
-		$("#filters .current").removeClass("current");
-		$(this).addClass("current");
-
-		var selector = $(this).attr("data-filter");
-
-		$(".items").isotope({
-			filter: selector,
-			animationOptions: {
-				duration: 1500,
-				easing: 'linear',
-				queue: false
-			}
-		});
-
-		return false;
-	});
-
-
-	$("#navigation li a").click(function(e) {
-		e.preventDefault();
-
-		var targetElement = $(this).attr("href");
-		var targetPosition = $(targetElement).offset().top;
-		$("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");
-
-	});
-
-
-	const nav = $("#navigation");
-	const navTop = nav.offset().top;
-
-	$(window).on("scroll", stickyNavigation);
-
-	function stickyNavigation() {
-
-		var body = $("body");
-
-		if($(window).scrollTop() >= navTop) {
-			body.css("padding-top", nav.outerHeight() + "px");
-			body.addClass("fixedNav");
-		}
-		else {
-			body.removeClass("fixedNav");
-			body.css("padding-top",0);
-		}
-
-
-
-
-	}
-
-
-
-
-
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    $('[data-fancybox]').fancybox({
+      transitionIn : 'fade',
+      transitionOut: 'fade',
+    });
+  
+    // code that scrolls down to the clicked link instead of jumping to it
+    $("#navigation li a").click(function(e) {
+      e.preventDefault();
+      var targetElement = $(this).attr("href");
+      var targetPosition = $(targetElement).offset().top;
+      $("html, body").animate({ scrollTop: targetPosition - 50 }, "slow");
+    });
+  
+    const nav = $("#navigation");
+    const navTop = nav.offset().top; //moment at which to add or remove the sticky class;
+    $(window).on("scroll", stickyNavigation);
+  
+    function stickyNavigation() {
+      var body = $("body");
+  
+      if($(window).scrollTop() >= navTop) {
+        body.css("padding-top", nav.outerHeight() - 10 + "px") //fix for content jumping on scroll lock trigger
+        body.addClass("fixedNav");
+      }
+      else {
+        body.css("padding-top", 0);
+        body.removeClass("fixedNav");
+      }
+    }
+  });
+  
